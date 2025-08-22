1f095060560213130243819d8a2432aa2b060796
alexklenio <alexlopes.see@gmail.com>
2025-08-22 16:20:22 -0300

    .

---
+++ 15 - 01 - strings.py
@@ -0,0 +1,27 @@
+nome = "aLeX"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+print()
+print()
+texto = "        Olá mundo   "
+
+print(texto , ".")
+print(texto.strip() , ".")
+print(texto.rstrip() , ".")
+print(texto.lstrip(), ".")
+
+print()
+print()
+
+menu = "Python"
+
+print('#####' ,menu, '#####')
+print(menu.center(18,'=' ))
+
+print()
+print()
+
+print('-'.join(menu))
