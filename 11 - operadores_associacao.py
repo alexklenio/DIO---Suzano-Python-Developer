cd15a29119c167ce7ac7a24c40f839d104fa6bb5
alexklenio <alexlopes.see@gmail.com>
2025-08-22 00:53:10 -0300

    Create 11 - operadores_associacao.py

---
+++ 11 - operadores_associacao.py
@@ -0,0 +1,10 @@
+curso = "Curso de Python"
+frutas = ["laranja", "limão", "uva"] 
+
+print("laranja" in frutas)
+
+print("Abacaxi" not in frutas)
+
+print("limão" not in frutas)
+
+print("Phyton" in curso)
