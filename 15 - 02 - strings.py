d2f973b88b36c0f7c4a11008a7854ee52f529357
alexklenio <alexlopes.see@gmail.com>
2025-08-22 16:48:00 -0300

    .

---
+++ 15 - 02 - strings.py
@@ -0,0 +1,6 @@
+nome = "Alex"
+idade = 39
+profissao = "Auditor"
+linguagem = "Phyton"
+
+print(f"Olá meu nome é {nome}, tenho {idade} anos, atualmente desenvolvo a função de {profissao} e estudo a linguagem {linguagem}.")
---
+++ 15 - 03 - strings.py
@@ -0,0 +1,17 @@
+nome = "Alex Klenio Ferreira Lopes"
+
+print(nome[0])
+
+print(nome[:11])
+
+print(nome[5:11])
+
+print(nome[6:11:2])
+
+print(nome[:])
+
+print(nome[::-1])
+
+
+
+
