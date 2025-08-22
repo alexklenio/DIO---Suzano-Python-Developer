f101dd486cd1794ddf07fde33b5d879c2bd08a80
alexklenio <alexlopes.see@gmail.com>
2025-08-21 23:17:54 -0300

    .

---
+++ 03 - variaveis_e_constantes.py
@@ -0,0 +1,11 @@
+nome = 'Alex Lopes'
+
+idade = 39
+
+nome, idade = 'Alex Klenio', 40
+
+print(nome, idade)
+
+ESTADOS_BRASILEIROS = ['SP', 'RJ', 'SC', 'RS']
+
+print(ESTADOS_BRASILEIROS)
---
+++ 04 - convertendo_tipos.py
@@ -0,0 +1,17 @@
+print(int(1.596))
+
+print(int('10'))
+
+print(float('10.10'))
+
+print(float(100))
+
+print(str(10.10))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(11/2)
+print(11//2)
