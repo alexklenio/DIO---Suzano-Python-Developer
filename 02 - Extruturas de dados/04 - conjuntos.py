86818ae6ccd4461ae9d855985c6c399583c74cf9
alexklenio <alexlopes.see@gmail.com>
2025-08-24 18:59:18 -0300

    .

--- 02 - Extruturas de dados/04 - conjuntos.py
+++ 02 - Extruturas de dados/04 - conjuntos.py
@@ -2,5 +2,17 @@ set('abacaxi')  #{'b', 'a', 'c', 'x', 'i'}
 
 set(('palio', 'gol', 'celta', 'palio')) #{'gol', 'celta', 'palio'}
 
-linguagens = {'python', 'java', 'python'}
+linguagens = {'python', 'java', 'python'} 
+
+linguagens.add('C#')
 print(linguagens)
+
+linguagens.discard('java') #remove o iten, se o mesmo não tiver ele continua
+linguagens.pop()
+linguagens.remove() #remove o iten, se o mesmo não tiver ele retorna uma mensagem de erro
+
+codigo = list(linguagens)
+print(codigo[0])
+
+
+print(codigo)
