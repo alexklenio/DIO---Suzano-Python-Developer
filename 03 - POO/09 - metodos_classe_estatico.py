f6a970e476eccc6ef88de090e199e92dfc383878
alexklenio <alexlopes.see@gmail.com>
2025-09-02 21:35:50 -0300

    Create 09 - metodos_classe_estatico.py

---
+++ 03 - POO/09 - metodos_classe_estatico.py
@@ -0,0 +1,21 @@
+class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
+    @classmethod
+    def criar_de_data_nascimento(cls, ano, mes, dia, nome):
+
+        print(cls)
+        idade = 2025 - ano 
+        return cls(nome, idade)
+    
+    @staticmethod
+    def e_maior_idade(idade):
+        return idade >= 18
+
+p = Pessoa.criar_de_data_nascimento(1986, 2, 12, "Alex")
+print(p.nome, p.idade)
+print()
+print(Pessoa.e_maior_idade(18))
+print(Pessoa.e_maior_idade(8))
