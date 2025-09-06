62319cb60339830e24c95ddc15bbe670759e48d7
alexklenio <alexlopes.see@gmail.com>
2025-09-06 20:14:23 -0300

    exercises

--- 05- manipulando_arquivos/01 - operacao_leitura.py
+++ 05- manipulando_arquivos/01 - operacao_leitura.py
@@ -5,3 +5,10 @@ arquivo = open('05- manipulando_arquivos\lorem.txt', "r")
 #print(arquivo.readlines())
 
 arquivo.close()
+
+try:
+    with open("05- manipulando_arquivos\lorem.txt", "r") as arquivo:
+        print("Trabalhando com o arquivo")
+
+except IOError as exc:
+    print(f"Erro ao abrir o arquivo {exc}")
--- 05- manipulando_arquivos/02 - operacao_escrita.py
+++ 05- manipulando_arquivos/02 - operacao_escrita.py
@@ -1,3 +1,7 @@
 arquivo = open("05- manipulando_arquivos/teste.txt", "w")
 
+
+arquivo.write("Escrevendo dados em um novo arquivo.")
+arquivo.writelines([" escrevendo ", "um " , "novo " , "texto"])
+
 arquivo.close()
