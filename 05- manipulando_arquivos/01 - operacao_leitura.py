dcd66eb8ac02fcd89c9ca89cc5cd9defbb4088c3
alexklenio <alexlopes.see@gmail.com>
2025-09-06 19:50:20 -0300

    operação escrita

---
+++ 05- manipulando_arquivos/01 - operacao_leitura.py
@@ -0,0 +1,7 @@
+arquivo = open('05- manipulando_arquivos\lorem.txt', "r")
+
+#print(arquivo.read())
+#print(arquivo.readline())
+#print(arquivo.readlines())
+
+arquivo.close()
---
+++ 05- manipulando_arquivos/02 - operacao_escrita.py
@@ -0,0 +1,3 @@
+arquivo = open("05- manipulando_arquivos/teste.txt", "w")
+
+arquivo.close()
