f5105e56e9c651378e059be4e00d813ae520f150
alexklenio <alexlopes.see@gmail.com>
2025-10-14 22:05:18 -0300

    ddelete

--- 07 - Banco de dados/01_conexao.py
+++ 07 - Banco de dados/01_conexao.py
@@ -6,6 +6,30 @@ ROOT_PATH = Path(__file__).parent
 conexao = sqlite3.connect(ROOT_PATH / "meu_banco.sqlite")
 cursor = conexao.cursor()
 
-cursor.execute(
-    "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(79), email VARCHAR(150))"
-)
+
+def criar_tabela(cursor):
+    cursor.execute(
+        "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
+    )
+    conexao.commit()
+
+
+def inserir_registro(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute("INSERT INTO clientes (nome, email) VALUES (?,?);", data)
+    conexao.commit()
+
+
+def atualizar_registro(conexao, cursor, nome, email, id):
+    data = (nome, email, id)
+    cursor.execute("UPDATE clientes SET nome=?, email=? WHERE id=?;", data)
+    conexao.commit()
+
+
+def excluir_registro(conexao, cursor, id):
+    data = (id,)
+    cursor.execute("DELETE FROM clientes WHERE id=?;", data)
+    conexao.commit()
+
+
+excluir_registro(conexao, cursor, 1)
